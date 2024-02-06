b9edc52057ca927b34e217b8172ef0cf5244aaed
Vinicius Macedo <macedo.vp@gmail.com>
2024-02-06 15:17:06 -0300

    Redo commit 8aa2ff6
    Use 'Autowired' annotation, see injection points and refactor fixing access modifier of AtivacaoClienteService attribute to private

--- algafood-api/src/main/java/com/macedovingithub/algafoodAPI/di/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/macedovingithub/algafoodAPI/di/service/AtivacaoClienteService.java
@@ -1,5 +1,6 @@
 package com.macedovingithub.algafoodAPI.di.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.macedovingithub.algafoodAPI.di.modelo.Cliente;
@@ -8,23 +9,39 @@ import com.macedovingithub.algafoodAPI.di.notificacao.Notificador;
 @Component
 public class AtivacaoClienteService {
 
-	public Notificador notificador;
-	
-	// Injetando dependência usando Construtor com argumentos
-	// Utilizando uma Interface para redução de acoplamento
-	public AtivacaoClienteService(Notificador notificador) {
-		
-		this.notificador = notificador;
-		
-		System.out.println("AtivacaoClienteService: " + notificador);
-	}
+	@Autowired
+	// Injetando dependência usando Atributo
+	private Notificador notificador;
+
+//	@Autowired
+//	// Injetando dependência usando Construtor com argumentos
+//	// Utilizando uma Interface para redução de acoplamento
+//	public AtivacaoClienteService(Notificador notificador) {
+//		
+//		this.notificador = notificador;
+//		
+//		System.out.println("AtivacaoClienteService: " + notificador);
+//	}
+
+//  Segundo CONSTRUTOR como exemplo -> Vai "confundir" o Spring
+//	public AtivacaoClienteService(String qualquer) {
+//		
+//	}
 
 	public void ativar(Cliente cliente) {
-		
+
 		System.out.println("Cliente ativado.");
 		
 		cliente.ativar();
-		
+
 		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
 	}
+
+//	@Autowired
+//	// Injetando dependência refrenciando um dos SETTERS
+//	public void setNotificador(Notificador notificador) {
+//		this.notificador = notificador;
+//	}
+
+
 }
